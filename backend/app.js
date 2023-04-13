6f64b8cd5b3e7dc1becd4e38a021ed5a92ec4961
Anastasiia Shaikina <shaikina@phystech.edu>
2023-04-13 19:04:29 +0200

    fix: исправть  cors

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,7 @@ app.use(function(req, res, next) {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   next();
